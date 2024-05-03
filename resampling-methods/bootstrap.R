0f00cfaaef4517d91391c0946add14d806455104
Andrea Rota <andrea.rota.98@gmail.com>
2024-05-03 16:30:42 +0200

    feat: splitted cleaning process from validation methods

--- resampling-methods/bootstrap.R
+++ resampling-methods/bootstrap.R
@@ -1,54 +1,14 @@
-install.packages("fastDummies")
-
+# Load required libraries
 library(readr)
-library(fastDummies)
 library(boot)
 library(dplyr)
 
+# Set the seed for reproducibility
+set.seed(123)
 
-set.seed(123) # Imposta il seed per la riproducibilità
-
-df <- read_csv("dataset/NY-House-Dataset 2.csv")
-
-## Missing data
-colSums(is.na(df))
-
-## Unique values
-sapply(df, function(x) n_distinct(x))
-
-
-## Remove unused column
-df <- df[, !(names(df) %in% c("ADDRESS", "STATE", "MAIN_ADDRESS", "ADMINISTRATIVE_AREA_LEVEL_2", "LOCALITY", "STREET_NAME", "LONG_NAME", "FORMATTED_ADDRESS"))]
-
-## Filter data
-
-vars <- c("PRICE","BEDS","BATH","PROPERTYSQFT")
-df_filtered <- df
-
-for (var in vars){
-  
-  Q1 <- quantile(df_filtered[[var]], 0.25)
-  Q3 <- quantile(df_filtered[[var]], 0.75)
-  
-  IQR <- Q3-Q1
-  lower_limit <- Q1-1.5*IQR
-  upper_limit <- Q3+1.5*IQR
-  
-  df_filtered <- df_filtered[df_filtered[[var]] >= lower_limit & df_filtered[[var]] <= upper_limit, ]
-  
-}
-
-## Dummies
-# Trasformazione delle variabili categoriche in dummy variables
-df <- dummy_cols(df_filtered)
-
-# Rimozione delle colonne originali delle variabili categoriche
-df <- df[, !(names(df) %in% c("BROKERTITLE", "TYPE", "SUBLOCALITY"))]
+df <- read_csv("dataset/NY-House-Dataset 2 - clean.csv")
 
 ##### LINEAR MULTIPLE REGRESSION ##### 
-lm_fit <- lm(BEDS ~ ., data = df[train,])
-summary(lm_fit)
-
 # perform bootstrap simulation
 fun_boot <- function(data,index){
   lm_fit <- lm(BEDS ~ ., data = data, subset = index)
--- resampling-methods/validation.R
+++ resampling-methods/validation.R
@@ -1,43 +1,10 @@
-install.packages("fastDummies")
-
+# Load required libraries
 library(readr)
-library(fastDummies)
 
-set.seed(123) # Imposta il seed per la riproducibilità
+# Set the seed for reproducibility
+set.seed(123)
 
-df <- read_csv("dataset/NY-House-Dataset 2.csv")
-
-## Missing data
-missing_values <- colSums(is.na(df))
-print(missing_values)
-
-## Remove unused column
-df <- df[, !(names(df) %in% c("ADDRESS", "STATE", "MAIN_ADDRESS", "ADMINISTRATIVE_AREA_LEVEL_2", "LOCALITY", "STREET_NAME", "LONG_NAME", "FORMATTED_ADDRESS"))]
-
-## Filter data
-
-vars <- c("PRICE","BEDS","BATH","PROPERTYSQFT")
-df_filtered <- df
-
-for (var in vars){
-  
-  Q1 <- quantile(df_filtered[[var]], 0.25)
-  Q3 <- quantile(df_filtered[[var]], 0.75)
-  
-  IQR <- Q3-Q1
-  lower_limit <- Q1-1.5*IQR
-  upper_limit <- Q3+1.5*IQR
-  
-  df_filtered <- df_filtered[df_filtered[[var]] >= lower_limit & df_filtered[[var]] <= upper_limit, ]
-  
-}
-
-## Dummies
-# Trasformazione delle variabili categoriche in dummy variables
-df <- dummy_cols(df_filtered)
-
-# Rimozione delle colonne originali delle variabili categoriche
-df <- df[, !(names(df) %in% c("BROKERTITLE", "TYPE", "SUBLOCALITY"))]
+df <- read_csv("dataset/NY-House-Dataset 2 - clean.csv")
 
 ## Test 30 - Train 70
 train <- sample(dim(df)[1], round(dim(df)[1]*0.7))
